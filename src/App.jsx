26e336bf1f6259b6326bc3d3a4255ae8aaf9b13b
Osvaldo Picazo <odpicazo@gmail.com>
2021-09-21 19:20:23 +0200

    [WDFT - Aug21 - RMT] Osvaldo Picazo - lab done to iteration 4

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import FormField from './components/FormField';
+import SignupForm from './components/SignupForm';
+
+function App() {
+  return (
+    <div className="App">
+     
+    <SignupForm />
+    </div> 
+  );
+}
+
+export default App;
